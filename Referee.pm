84cb12c0b5b7adeec86601bbb6dca6636bed4469
Gregor N. Purdy, Sr <gnp@acm.org>
2001-04-13 13:54:41 +0000

    Imported 0.002 code.

--- Referee.pm
+++ Referee.pm
@@ -1,134 +1,43 @@
 #
-# Copyright (C) 1999 Gregor N. Purdy
-# All rights reserved.
+# Games::Chess::Referee
+#
+# A Perl Module for validating chess moves.
+#
+# Copyright (C) 1999 Gregor N. Purdy. All rights reserved.
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl iteself.
 #
 
 package Games::Chess::Referee;
+
 use base 'Exporter';
 use strict;
 use vars qw($VERSION @EXPORT @EXPORT_OK %EXPORT_TAGS);
-$VERSION = '0.001';
+$VERSION = '0.002';
 @EXPORT = qw(&ply &move &new_game &show_board);
 @EXPORT_OK = @EXPORT;
 
+use Games::Chess qw(:constants :functions);
+use Carp;
+
 my $board;
-my $move;
-my $to_move;
-
-my $white = 0;
-my $black = 1;
-
-my $white_may_castle_short;
-my $white_may_castle_long;
-my $black_may_castle_short;
-my $black_may_castle_long;
 
 sub upper { my ($s) = @_; $s =~ tr/a-z/A-Z/; return $s; };
 sub lower { my ($s) = @_; $s =~ tr/A-Z/a-z/; return $s; };
 
-sub white { my ($s) = @_; $s =~ tr/a-z/A-Z/; return $s; };
-sub black { my ($s) = @_; $s =~ tr/A-Z/a-z/; return $s; };
-
 my $occupy  = '-';
 my $capture = 'x';
-
-my $empty   = ' ';
-my $pawn    = 'P';
-my $rook    = 'R';
-my $knight  = 'N';
-my $bishop  = 'B';
-my $queen   = 'Q';
-my $king    = 'K';
-
-
-#
-# is_empty()
-#
-
-sub is_empty
-{
-	my ($s) = @_;
-	return $s eq $empty;
-}
-
-
-#
-# is_white()
-#
-
-sub is_white
-{
-	my ($s) = @_;
-	return $s =~ m/[PRNBQK]/;
-}
-
-
-#
-# is_black()
-#
-
-sub is_black
-{
-	my ($s) = @_;
-	return $s =~ m/[prnbqk]/;
-}
-
-
-#
-# is_pawn()
-#
-
-sub is_pawn
-{
-	my ($s) = @_;
-	return $s =~ m/[pP]/;
-}
 
 
 #
 # new_game()
 #
 
-sub new_game
+sub new_game ()
 {
-	my ($file, $rank);
+	$board = Games::Chess::Position->new();
 
-	undef $board;
-
-	foreach $file ('a' ... 'h') {
-		$board->{$file}{2} = white($pawn);
-		$board->{$file}{7} = black($pawn);
-		foreach $rank (3 ... 6) { $board->{$file}{$rank} = $empty; }
-	}
-
-	$board->{'a'}{1} = white($rook);
-	$board->{'h'}{1} = white($rook);
-	$board->{'a'}{8} = black($rook);
-	$board->{'h'}{8} = black($rook);
-
-	$board->{'b'}{1} = white($knight);
-	$board->{'g'}{1} = white($knight);
-	$board->{'b'}{8} = black($knight);
-	$board->{'g'}{8} = black($knight);
-
-	$board->{'c'}{1} = white($bishop);
-	$board->{'f'}{1} = white($bishop);
-	$board->{'c'}{8} = black($bishop);
-	$board->{'f'}{8} = black($bishop);
-
-	$board->{'d'}{1} = white($queen);
-	$board->{'e'}{1} = white($king);
-	$board->{'d'}{8} = white($queen);
-	$board->{'e'}{8} = white($king);
-
-	$to_move = $white;
-
-	$white_may_castle_short = 1;
-	$white_may_castle_long  = 1;
-	$black_may_castle_short = 1;
-	$black_may_castle_long  = 1;
-
-	$move = 1;
+#	print STDERR "Game: ", $board->to_FEN(), "\n";
 }
 
 
@@ -136,48 +45,15 @@ sub new_game
 # show_board()
 #
 
-sub show_board
-{
-	my ($rank, $file, $square);
-
-	print "\n";
-
-	my @ranks = (8, 7, 6, 5, 4, 3, 2, 1);
-	my @files = qw(a b c d e f g h);
-
-	print "\n";
-
-	foreach $rank (@ranks) {
-		foreach $file (@files) {
-			$square = $board->{$file}{$rank};
-			$square = ' ' unless defined($square);
-
-			print $square;
-		}
-		print "\n";
-	}
-
-	return;
-}
-
-
-#
-# occupy()
-#
-
-sub occupy
-{
-	my ($file, $rank, $piece, $color) = @_;
-
-	
-}
+sub show_board ()
+	{ print $board->to_text(), "\n"; }
 
 
 #
 # ply()
 #
 
-sub ply
+sub ply ($)
 {
 	my ($ply) = @_;
 	my ($piece, $ff, $fr, $act, $tf, $tr, $note);
@@ -188,26 +64,40 @@ sub ply
 	#
 
 	if ($ply eq '0-0') {
-		if ($to_move == $white) { $ply = 'e1g1'; }
-		else                    { $ply = 'e8g8'; }
+		if ($board->player_to_move() eq &WHITE) { $ply = 'E1G1'; }
+		else                                    { $ply = 'E8G8'; }
 	}
 	elsif ($ply eq '0-0-0') {
-		if ($to_move == $white) { $ply = 'e1c1'; }
-		else                    { $ply = 'e8c8'; }
+		if ($board->player_to_move() eq &WHITE) { $ply = 'E1C1'; }
+		else                                    { $ply = 'E8C8'; }
 	}
 
 	#
 	# Parse the ply notation:
 	#
 
-	if ($ply =~ m/^([PRNBQK]|)([a-h])([1-8])(x|-|)([a-h])([1-8])(.*)$/) {
-		($piece, $ff, $fr, $act, $tf, $tr, $note) = ($ply =~ m/^([PRNBQK]|)([a-h])([1-8])(x|-|)([a-h])([1-8])(.*)$/);
+	if ($ply =~ m/^([prnbqkPRNBQK]|)([a-hA-H])([1-8])(x|-|)([a-hA-H])([1-8])(.*)$/) {
+		($piece, $ff, $fr, $act, $tf, $tr, $note) = ($ply =~ m/^([prnbqkPRNBQK]|)([a-hA-H])([1-8])(x|-|)([a-hA-H])([1-8])(.*)$/);
 		$piece = upper($piece);
+		$ff    = upper($ff);
+		$tf    = upper($tf);
 	}
 	else {
-		die "Unsupported notation: '$ply'!\n";
+		carp "Unsupported notation: `$ply'!";
+		return 0;
 	}
 
+	my $from = lower("$ff$fr");
+	my $to   = lower("$tf$tr");
+
+	my @from = algebraic_to_xy($from);
+	my @to   = algebraic_to_xy($to);
+
+	my $from_piece = $board->at(@from);
+	my $to_piece   = $board->at(@to);
+
+	my $from_kind  = upper($from_piece->code());
+	my $to_kind    = upper($to_piece->code());
 
 	#
 	# Check for attempts to castle:
@@ -221,86 +111,170 @@ sub ply
 	# relevant squares are under attack.
 	#
 
-	if	($ff eq 'e' and $fr == 1 and $tf eq 'g' and $tr == 1) {
-		die "Castling short not permitted!\n" unless $white_may_castle_short;
-		die "Way not clear for castling short!\n" unless is_space($board->{'f'}{1});
-		die "Way not clear for castling short!\n" unless is_space($board->{'g'}{1});
-		$board->{'f'}{1} = $board->{'g'}{1};
-		$board->{'g'}{1} = $empty;
-		$notation = '0-0';
+	my $castling;
+
+	if	($ff eq 'E' and $fr == 1 and $tf eq 'G' and $tr == 1) {
+#		print STDERR "ATTEMPT BY WHITE TO CASTLE SHORT...\n";
+
+		if (!$board->can_castle(&WHITE, &KING)) {
+			carp "Castling short by white not permitted!";
+#			print STDERR "Game = ", $board->to_FEN(), "\n";
+			return 0;
+		} elsif (!$board->at(5, 0)->code() eq ' ') {
+			carp "Way not clear (space `f1') for castling short!";
+			return 0;
+		} elsif (!$board->at(6, 0)->code() eq ' ') {
+			carp "Way not clear (space `g1') for castling short!";
+			return 0;
+		} else {
+			$board->at(5, 0, $board->at(7, 0));
+			$board->at(7, 0, Games::Chess::Piece->new);
+
+			$notation = '0-0';
+			$castling = 'SHORT';
+		}
 	}
-	elsif	($ff eq 'e' and $fr == 8 and $tf eq 'g' and $tr == 8) {
-		die "Castling short not permitted!\n" unless $black_may_castle_short;
-		die "Way not clear for castling short!\n" unless is_space($board->{'f'}{8});
-		die "Way not clear for castling short!\n" unless is_space($board->{'g'}{8});
-		$board->{'f'}{8} = $board->{'g'}{8};
-		$board->{'g'}{8} = $empty;
-		$notation = '0-0';
+	elsif	($ff eq 'E' and $fr == 8 and $tf eq 'G' and $tr == 8) {
+#		print STDERR "ATTEMPT BY BLACK TO CASTLE SHORT...\n";
+
+		if (!$board->can_castle(&BLACK, &KING)) {
+			carp "Castling short by black not permitted!";
+			return 0;
+		} elsif (!$board->at(5, 7)->code() eq ' ') {
+			carp "Way not clear (space `f8') for castling short!";
+			return 0;
+		} elsif (!$board->at(6, 7)->code() eq ' ') {
+			carp "Way not clear (space `g8') for castling short!";
+			return 0;
+		} else {
+			$board->at(5, 7, $board->at(7, 7));
+			$board->at(7, 7, Games::Chess::Piece->new);
+
+			$notation = '0-0';
+			$castling = 'SHORT';
+		}
 	}
-	elsif	($ff eq 'e' and $fr == 1 and $tf eq 'c' and $tr == 1) {
-		die "Castling long not permitted!\n" unless $white_may_castle_long;
-		die "Way not clear for castling long!\n" unless is_space($board->{'b'}{1});
-		die "Way not clear for castling long!\n" unless is_space($board->{'c'}{1});
-		die "Way not clear for castling long!\n" unless is_space($board->{'d'}{1});
-		$board->{'d'}{1} = $board->{'a'}{1};
-		$board->{'a'}{1} = $empty;
-		$notation = '0-0-0';
+	elsif	($ff eq 'E' and $fr == 1 and $tf eq 'C' and $tr == 1) {
+#		print STDERR "ATTEMPT BY WHITE TO CASTLE LONG...\n";
+
+		if (!$board->can_castle(&WHITE, &QUEEN)) {
+			carp "Castling long by white not permitted!";
+			return 0;
+		} elsif (!$board->at(1, 0)->code() eq ' ') {
+			carp "Way not clear (space `b1') for castling long!";
+			return 0;
+		} elsif (!$board->at(2, 0)->code() eq ' ') {
+			carp "Way not clear (space `c1') for castling long!";
+			return 0;
+		} elsif (!$board->at(3, 0)->code() eq ' ') {
+			carp "Way not clear (space `d1') for castling long!";
+			return 0;
+		} else {
+			$board->at(3, 0, $board->at(0, 0));
+			$board->at(0, 0, Games::Chess::Piece->new);
+
+			$notation = '0-0-0';
+			$castling = 'LONG';
+		}
 	}
-	elsif	($ff eq 'e' and $fr == 8 and $tf eq 'c' and $tr == 8) {
-		die "Castling long not permitted!\n" unless $black_may_castle_long;
-		die "Way not clear for castling long!\n" unless is_space($board->{'b'}{8});
-		die "Way not clear for castling long!\n" unless is_space($board->{'c'}{8});
-		die "Way not clear for castling long!\n" unless is_space($board->{'d'}{8});
-		$board->{'d'}{8} = $board->{'a'}{8};
-		$board->{'a'}{8} = $empty;
-		$notation = '0-0-0';
+	elsif	($ff eq 'E' and $fr == 8 and $tf eq 'C' and $tr == 8) {
+#		print STDERR "ATTEMPT BY BLACK TO CASTLE LONG...\n";
+
+		if (!$board->can_castle(&BLACK, &QUEEN)) {
+			carp "Castling long by black not permitted!";
+			return 0;
+		} elsif (!$board->at(1, 0)->code() eq ' ') {
+			carp "Way not clear (space `b8') for castling long!";
+			return 0;
+		} elsif (!$board->at(2, 0)->code() eq ' ') {
+			carp "Way not clear (space `c8') for castling long!";
+			return 0;
+		} elsif (!$board->at(3, 0)->code() eq ' ') {
+			carp "Way not clear (space `d8') for castling long!";
+			return 0;
+		} else {
+			$board->at(3, 7, $board->at(0, 7));
+			$board->at(0, 7, Games::Chess::Piece->new);
+
+			$notation = '0-0-0';
+			$castling = 'LONG';
+		}
 	}
-	
+	else {
+		# Not castling.
+	}
+
 	#
 	# Record new castling permissions:
 	#
+	# TODO: Write tests that exercise this code! The warnings weren't printing
+	# when they should.
+	#
 
-	if    ($ff eq 'a' and $fr == 1) { $white_may_castle_long  = 0; } # Queen's Rook
-	elsif ($ff eq 'a' and $fr == 8) { $black_may_castle_long  = 0; }
-	elsif ($ff eq 'h' and $fr == 1) { $white_may_castle_short = 0; } # King's Rook
-	elsif ($ff eq 'h' and $fr == 8) { $black_may_castle_long  = 0; }
-	elsif ($ff eq 'e' and $fr == 1) {
-		$white_may_castle_short = 0;
-		$white_may_castle_long  = 0;
-	}
-	elsif ($ff eq 'e' and $fr == 8) {
-		$black_may_castle_short = 0;
-		$black_may_castle_long  = 0;
+	if      ($from eq 'A1') {
+		$board->can_castle(&WHITE, &QUEEN, 0);
+#		print STDERR "Warning: Castling long by white no longer permitted.\n";
+	} elsif ($from eq 'A8') {
+		$board->can_castle(&BLACK, &QUEEN, 0);
+#		print STDERR "Warning: Castling long by black no longer permitted.\n";
+	} elsif ($from eq 'H1') {
+		$board->can_castle(&WHITE, &KING,  0);
+#		print STDERR "Warning: Castling short by white no longer permitted.\n";
+	} elsif ($from eq 'H8') {
+		$board->can_castle(&BLACK, &KING,  0);
+#		print STDERR "Warning: Castling short by black no longer permitted.\n";
+	} elsif ($from eq 'E1') {
+		$board->can_castle(&WHITE, &QUEEN, 0);
+		$board->can_castle(&WHITE, &KING,  0);
+#		print STDERR "Warning: Castling short by white no longer permitted.\n";
+#		print STDERR "Warning: Castling long by white no longer permitted.\n";
+	} elsif ($from eq 'E8') {
+		$board->can_castle(&BLACK, &QUEEN, 0);
+		$board->can_castle(&BLACK, &KING,  0);
+#		print STDERR "Warning: Castling short by black no longer permitted.\n";
+#		print STDERR "Warning: Castling long by black no longer permitted.\n";
+	} else {
+		# No change to castling status.
 	}
 
 	#
 	# Detect the piece: 
 	#
 
-	if ($piece and (upper($piece) ne upper($board->{$ff}{$fr}))) {
-		die "Ply piece '$piece' does not match board piece (",
-			upper($board->{$ff}{$fr}), ") at $ff$fr!\n";
-	}
+	if (!$piece) { $piece = $from_kind; };
 
-	$piece = $board->{$ff}{$fr};
+	if ($piece ne $from_kind) {
+#		print STDERR "\n";
+#		print STDERR "Piece: $piece\n";
+#		print STDERR "Ply:   $ply\n";
+#		print STDERR "From Space: $from\n";
+#		print STDERR "From Kind: $from_kind\n";
+		carp "Piece (`$piece') from ply (`$ply') does not match board piece (`$from_kind') at space `$from'!";
+		return 0;
+	}
 
 	#
 	# Detect the action:
 	#
+	# TODO: Make sure we only permit capture of other color's pieces.
+	#
 
-	if (is_empty($board->{$tf}{$tr})) {
-		if ($act eq $capture) { die "Ply capture (__x__) does not match board!\n"; }
-		$act = $occupy;
-	}
-	else {
-		if ($act eq $occupy)  { die "Ply occupy (__-__) does not match board!\n"; }
-		$act = $capture;
+	my $board_act;
+
+	if ($to_kind eq ' ') { $board_act = $occupy; }
+	else                 { $board_act = $capture; }
+
+	if (!$act) { $act = $board_act; }
+
+	if ($act ne $board_act) {
+		carp "Action (`$act') from ply (`$ply') does not match board (space `$to' contains `$to_kind')!";
+		return 0;
 	}
 
 	#
 	# Effect the move:
 	#
-	# TODO: Deal with castle capabilities and en passant target.
+	# TODO: Deal with en passant target.
 	# TODO: Detect check and checkmate for notes (and validate against those
 	# passed in, if any).
 	# TODO: Detect en passant capture for notes.
@@ -310,29 +284,34 @@ sub ply
 	# TODO: Detect forced for notes.
 	#
 
-	$board->{$tf}{$tr} = $board->{$ff}{$fr};
-	$board->{$ff}{$fr} = $empty;
+	$board->at(@to, $from_piece);
+	$board->at(@from, Games::Chess::Piece->new);
 
 	#
 	# Print the move:
 	#
 
 	if (!defined $notation) {
-		if (is_pawn($piece)) { $piece = $empty; }
-		$notation = $piece . $ff . $fr . $act . $tf . $tr . $note;
+		if ($from_kind eq 'P') { $piece = ' '; }
+		$notation = $piece . lower($from) . $act . lower($to) . $note;
+		$notation = $notation . (' ' x (8 - length($notation)));
 	}
 
-	if ($to_move == $white) {
-		print "$move. $notation ";
-		$to_move = $black;
-		$ply++;
+	if ($board->player_to_move() == &WHITE) {
+		print $board->move_number(), ". $notation ";
+
+		$board->player_to_move(&BLACK);
+		$board->halfmove_clock($board->halfmove_clock() + 1);
 	}
 	else {
 		print "$notation\n";
-		$to_move = $white;
-		$ply++;
-		$move++;
+
+		$board->player_to_move(&WHITE);
+		$board->halfmove_clock($board->halfmove_clock() + 1);
+		$board->move_number($board->move_number() + 1);
 	}
+
+	return 1;
 }
 
 
@@ -340,10 +319,19 @@ sub ply
 # move()
 #
 
-sub move
+sub move ($$)
 {
-	ply $_[0];
-	ply $_[1];
+	if (!&ply($_[0])) {
+		carp "First ply (`$_[0]') of move failed.";
+		return 0;
+	}
+
+	if (!&ply($_[1])) {
+		carp "Second ply (`$_[1]') of move failed.";
+		return 0;
+	}
+
+	return 1;
 }
 
 
